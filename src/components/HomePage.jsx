3388f561a526a54746103dcf67d03fc6ffffcdf6
DzenyTech <dzenana.sehic.24@size.ba>
2025-09-06 13:00:33 +0200

    Additional files/Maping

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,18 +3,42 @@ import { useState, useEffect } from 'react'
 const HomePage = () => {
     const API_URL = "https://api.rawg.io/api/games";
     const API_KEY ="1e5139d26e0449aeaee838bd6fd1fa75";
+   const [games, setGames]=useState([])
 useEffect(() => {
     
     const fetchGames = async () => {
-        const response = await fetch(`${API_URL}?key=${API_KEY}`);
-        const data = await response.json();
-        console.log(data);
-    };
-    fetchGames();
-}, []);
+       try {
+                const response = await fetch(`${API_URL}?key=${API_KEY}`);
+                const data = await response.json();
+                console.log(data);  
+                
+                if (Array.isArray(data.results)) {
+                    setGames(data.results);  
+                } else {
+                    console.error("Expected 'results' array not found in the API response");
+                }
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        
+        fetchGames();
+    }, []); 
+    
+    
     return ( 
-        <div>HomePage</div  >
+            <div className="game-card">
+            {games.map((game) => (
+                <div key={game.id}>
+                    <a href={game.background_image} target="_blank" rel="noopener noreferrer">
+                        <img src={game.background_image} alt={game.name} />
+                    </a>
+                    <p>{game.name}</p>
+                </div>
+            ))}
+        </div>
+    
      );
-}
+};
  
 export default HomePage;
